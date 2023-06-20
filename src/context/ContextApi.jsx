572a733aa6926f816914d2e9a845541a96628d9f
Yashraj Singh Mandloi <yashrajsinghmandloi@gmail.com>
2023-06-21 02:05:08 +0530

    clone

---
+++ src/context/ContextApi.jsx
@@ -0,0 +1,47 @@
+import React, { createContext, useEffect, useState } from "react";
+
+import { fetchDataFromApi } from "../utils/api";
+
+export const Context = createContext();
+
+export const AppContext = (props) =>{
+
+    const[loading,setLoading] = useState(false);
+    const[SearchResults,setSearchResults] = useState(false);
+    const[selectCategories,setSelectCategories] = useState("New");
+    const[mobileMenu,setMobileMenu] = useState(false);
+
+    useEffect(()=>{
+        fetchSelectedCategoryData(selectCategories)
+    },[selectCategories])
+
+    const fetchSelectedCategoryData = (query)=>
+    {
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`).then(({contents}) =>{
+            console.log(contents);
+            setSearchResults(contents);
+            setLoading(false)
+            
+        })
+    }
+
+    return (
+
+        <Context.Provider value={{
+            loading,
+            setLoading,
+            SearchResults,
+            setSearchResults,
+            selectCategories,
+            setSelectCategories,
+            mobileMenu,
+            setMobileMenu
+        }}
+        >
+            {props.children}
+        </Context.Provider>
+
+    );
+}
+
